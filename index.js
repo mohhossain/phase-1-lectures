26cd3b7cd3c2dee90dc7a81bf638b2aa128e256d
Mohammad <moh.hossain98@gmail.com>
2023-10-25 17:25:56 -0400

    starter

---
+++ index.js
@@ -0,0 +1,91 @@
+// Deliverables
+
+// JavaScript Fundamentals and Data Structures Workshop
+
+// Workshop Introduction:
+// In this workshop, we'll cover JavaScript fundamentals and data structures to help you build a strong foundation.
+// Let's get started!
+
+// Variables and Data Types
+
+// 1. Declare a variable 'age' and assign your age to it.
+// 2. Declare a constant 'name' and assign your name to it.
+// 3. Declare a variable 'isStudent' and assign a boolean value to it.
+// 4. Declare a variable 'score' and assign a numeric value to it.
+// 5. Declare an array 'fruits' and add a few different fruit names to it.
+// 6. Declare an object 'person' with properties 'name' and 'age'.
+
+// Console Output
+
+// 7. Use 'console.log' to display a welcome message to the console.
+
+// Math Operations
+
+// 8. Perform addition and multiplication operations on two numbers and store the results in variables.
+
+// Strings
+
+// 9. Create a string variable 'greeting'
+// 10. Use string interpolation to create a message by combining 'greeting' and 'name'.
+
+// Conditional Statements
+
+// 11. Write an 'if' statement to check if your age is greater than or equal to 18.
+
+// 12. Use 'if-else' to display different messages based on isStudent value.
+// a. If isStudent is true, display 'Welcome to Flatiron!'
+// b. If isStudent is false, display 'Welcome!'
+
+// Loops
+
+// 13. Use a 'for' loop to print numbers from 1 to 5.
+// 14. Use a 'while' loop to print numbers from 1 to 5.
+
+// Arrays
+
+// 15. Declare an array 'colors' with a few color names.
+
+let colors = [
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "orange",
+  "purple",
+  "pink",
+  "black",
+  "white",
+  "brown",
+];
+
+// 16. Add a new color to the 'colors' array.
+
+colors.push("grey");
+
+// 17. Access the first and last color in the array.
+
+console.log(colors[0]);
+
+// access the color at a specific index.
+
+// find the index of a color.
+
+colors.indexOf("red");
+
+// check if a color exists in the 'colors' array.
+// remove the last color from the 'colors' array.
+// remove the first color from the 'colors' array.
+// remove a color from the 'colors' array at a specific index.
+
+// Objects
+
+// 18. Declare an object 'car' with properties 'make', 'model', and 'year'.
+// 19. Access and modify the 'year' property of the 'car' object.
+// 20. Iterate through the properties of the 'car' object.
+
+// Data Structures
+
+// 21. Use array methods to remove an element, add an element at the beginning, and find the index of an element.
+// 22. Create a set to store unique values.
+// 23. Create a map to store key-value pairs.
+// 24. Use the 'Set' and 'Map' data structures with some operations.
