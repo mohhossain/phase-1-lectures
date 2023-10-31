ee6b9c8644f3acd051cc5c278848c6a7ca387737
Mohammad <moh.hossain98@gmail.com>
2023-10-31 12:21:15 -0400

    solution

--- index.js
+++ index.js
@@ -6,21 +6,70 @@
 
 // *let's create a function that will print out a message to the console.
 
-function printMessage() {
-  console.log("Hello World!");
+printMessage("Hello");
+
+function printMessage(message) {
+  console.log(message);
 }
 
-printMessage();
-
+printMessage("Hello world");
+printMessage("Good Morning!");
 // output: Hello World!
 
 // pass a custom parameter to the function
 
 // create a function that takes two numbers parameters and returns the sum of those parameters
+function sum(num1, num2) {
+  console.log("Done!");
+  return num1 + num2;
+}
 
+console.log("Done!");
+// return statement:
+// 1. returns a value, this value can be used anywhere in the code
+//
+
+let result = sum(20, 30);
+let result2 = sum(19, 21);
+
+console.log(result);
+console.log(result2);
+
+sum(2, 3);
 // create a function that takes two numbers and a string for the operator and then returns the result of the operator on the two numbers.
 
+function calculate(num1, num2, operator) {
+  if (operator === "*") {
+    return num1 * num2;
+  } else if (operator === "-") {
+    return num1 - num2;
+  } else if (operator === "+") {
+    return num1 + num2;
+  } else if (operator === "/") {
+    return num1 / num2;
+  } else {
+    return "Not a valid operation!";
+  }
+
+  //   switch (operator) {
+  //     case "+": {
+  //       return num1 + num2;
+  //     }
+  //     case "-":
+  //       return num1 - num2;
+  //   }
+}
+
 /* 
+
+calculate(2, 5, "*")
+=> 10 
+calculate(10, 5, "+")
+=> 15
+calculate(20, 2, "/")
+=> 10
+
+
 ! student exercise:
 ? create a function `rectangleAreaCalculator` that takes the length and width of a rectangle and returns the area of the rectangle. (formula: length * width)
 ? create a function  that takes the length and width of a rectangle and returns the perimeter of the rectangle. (formula: 2 * (length + width)
@@ -29,6 +78,13 @@ printMessage();
 
 // ! function scope
 // * variables declared inside a function are only accessible inside that function - Local Scope
+
+// let message = "Good Morning!";
+// function greeting() {
+//   console.log(message);
+// }
+
+// console.log(message);
 // * variables declared outside of a function are accessible anywhere in the file - Global Scope
 
 // ! first class functions - functions as variables
@@ -43,6 +99,12 @@ printMessage();
 // let avg = average;
 
 // console.log(avg(5, 10));
+// console.log(average(20, 20));
+// average(20, 20);
+
+let average = function (num1, num2) {
+  return (num1 + num2) / 2;
+};
 
 // * functions can be passed as arguments to other functions - callback functions
 
@@ -52,14 +114,34 @@ printMessage();
 //   console.log(funcArg, name);
 // }
 
+let goodMorning = function () {
+  return "Good morning!";
+};
+
+function goodAfternoon() {
+  return "Good afternoon";
+}
+
+function goodBye() {
+  return "Good bye!";
+}
+
+// function sum() {
+//   return 1;
+// }
+
+greeting("Hello", "Mohammad");
+
+greeting(sum(1, 2), "Mohammad");
+
 // * functions can be returned from other functions
 
 // example:
-// function greeting() {
-//   return function () {
-//     console.log("Hello World!");
-//   };
-// }
+function greeting() {
+  return sum(1, 2);
+}
+
+console.log(greeting());
 
 // ! student exercise: callback functions
 // ? create a function `calculate` that takes two numbers and a function as argument and then executes the function on the two numbers passed in.
@@ -68,14 +150,32 @@ printMessage();
 // * arrow functions are a shorter syntax for writing function expressions
 // example:
 
+let hello = () => console.log("Hello");
+
+// function hello() {
+//   console.log("Hello");
+// }
+
+// function sum2(num1, num2) {
+//   return num1 + num2;
+// }
+
 // * rules:
 
 // * if the function has a single parameter, the parentheses are optional
 // example:
 
+// let hello = (message) => console.log(message);
+
 // * if the function has a single expression, the curly braces are optional
 // example:
+// let hello = (message) => console.log(message);
 
-// * if the function has a single expression, the return keyword is optional
+// * if the function has a single expression, the return keyword is not allowed
 // example:
+
+const sum2 = (num1, num2) => {
+  console.log("Getting the numbers...");
+  return num1 + num2;
+};
 
