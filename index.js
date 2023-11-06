d9245e60dce19199d77dc9d3632fc176a284c7c7
Mohammad <moh.hossain98@gmail.com>
2023-11-06 12:10:58 -0500

    get/post

--- index.js
+++ index.js
@@ -4,7 +4,79 @@
 
 // * Deliverable 1: get all the posts from localhost:3000/posts and render them on the page, each post should have a <img/>, <p> for caption, a <button>❤️</button> for likes, and a <p> for likes count
 
+fetch("http://localhost:3000/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    // data.forEach(function (post) {
+    //   console.log(post);
+    // });
+
+    data.forEach(renderPost);
+
+    // for (let post of data) {
+    //   console.log(post);
+    //   renderPost(post);
+    // }
+  });
+
+// img -> for image
+// p -> caption
+// button with ❤️ for like button
+// p -> like count
+
+let feed = document.querySelector(".feed");
+
+function renderPost(post) {
+  let img = document.createElement("img");
+  img.src = post.image;
+
+  let p = document.createElement("p");
+  p.textContent = post.caption;
+
+  let button = document.createElement("button");
+  button.textContent = "❤️";
+
+  let likeCount = document.createElement("p");
+  likeCount.textContent = post.likes.count + " likes";
+
+  feed.append(img, p, button, likeCount);
+}
+
 // * Deliverable 2: Using the form, create a new post making a post request and add it to the page without having to refresh
+
+let form = document.querySelector(".post-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log(e.target[0].value, e.target[1].value);
+
+  let newPost = {
+    image: e.target[1].value,
+    caption: e.target[0].value,
+    likes: {
+      count: 0,
+    },
+    shares: {
+      count: 0,
+    },
+  };
+
+  fetch("http://localhost:3000/posts", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(newPost),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      renderPost(data);
+    });
+}
 
 // * Deliverable 3: When a user clicks the like button, the button should update the number of likes on the page without having to refresh and should also update the number of likes in the backend
 
